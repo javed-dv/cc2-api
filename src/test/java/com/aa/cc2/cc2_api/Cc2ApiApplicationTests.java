dc2c8fc4edb41fb23d8c867c702bf638154eb78d
javed-dv <javed.akhtar.dv@gmail.com>
2024-09-16 14:47:32 -0500

    initial checkin

---
+++ src/test/java/com/aa/cc2/cc2_api/Cc2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa.cc2.cc2_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cc2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
